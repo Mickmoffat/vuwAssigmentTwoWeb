9b9f828772d908fb3f079785c21512ac2ca978c2
Mickmoffat <mickmoffat@gmail.com>
2015-08-25 21:39:25 +1200

    7.2 - newsDisplay
    News display items at all times, before changing placement of headers

--- disaster Zone/disaster Zone/style/javaScryptCustom.js
+++ disaster Zone/disaster Zone/style/javaScryptCustom.js
@@ -24,13 +24,13 @@ $(document).ready(function () {
     $(".navNews").click(function () {
         //show nav expand div
         $("#navNExpand").removeClass("hidden");
-        $("#navNExpand").animate({ height: "100%" }); //sets hight and animates
+        $("#navNExpand").animate({ height: "100vh" }); //sets hight and animates
         clickN = clickN + 1; //adds 1 to var
 
         //hide nav expand div
         if (clickN == 2) {
             $("#navNExpand").addClass("hidden");
-            $("#navNExpand").animate({ height: "0%" }); //sets hight to 0%
+            $("#navNExpand").animate({ height: "0vh" }); //sets hight to 0%
             clickN = 0;
         }
 

c566907a2612de1005a29d03f3de7068eeae330e
Mickmoffat <mickmoffat@gmail.com>
2015-08-25 12:39:44 +1200

    7.2 - jQuery Hide Show Nav
    Hide and show the hidden elements, for nav more info and news

--- disaster Zone/disaster Zone/style/javaScryptCustom.js
+++ disaster Zone/disaster Zone/style/javaScryptCustom.js
@@ -1,1 +1,35 @@
-/* javaScrypt */
+/* custom javaScrypt MDDN201 P2 [2015] (300317288)*/
+
+$(document).ready(function () {
+/**** NAV CONTENT HIDE SHOW ****/
+    var click = 0; // var counter to show or hide #hidden ID 
+
+/*++++ NEWS ++++*/
+    
+    $(".navNews").click(function () {
+        //show nav expand div
+        $("#navNExpand").removeClass("hidden");
+        click = click += 1;
+
+        //hide nav expand div
+        if (click == 2) {
+            $("#navNExpand").addClass("hidden");
+            click = 1;
+        }
+        /* $("#navNExpand").removeClass("hidden");
+        click = click += 1;
+
+        if (click == 2) {
+            $("#navNExpand").addClass("hidden");
+            click = 1;
+        }*/
+    });
+
+/*+++ HELP OUT +++*/
+
+/*++ PREPARE ++*/
+
+/*+ MORE INFO +*/
+
+    
+});

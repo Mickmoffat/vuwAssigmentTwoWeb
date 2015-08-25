eb8426f97bd67c849979360d34db3957dd319a2a
Mickmoffat <mickmoffat@gmail.com>
2015-08-25 19:50:48 +1200

    7.2-  dropDownMenu working
    Pre tiny scrollbar, and google API

--- disaster Zone/disaster Zone/style/javaScryptCustom.js
+++ disaster Zone/disaster Zone/style/javaScryptCustom.js
@@ -1,6 +1,16 @@
 /* custom javaScrypt MDDN201 P2 [2015] (300317288)*/
 
 $(document).ready(function () {
+    /***** TINY SCROLL ****
+    var $box = $("#box");
+    $box.tinyscrollbar();
+
+    var box = $box.data();
+
+    $box.bind("move", function () {
+
+    }); */
+
     /**** NAV CONTENT HIDE SHOW ****/
 /* CLICK VAR */
     // var counter to show or hide #hidden ID
@@ -26,11 +36,56 @@ $(document).ready(function () {
 
     });
 
-/*+++ HELP OUT +++*/
+ /*+++ HELP OUT +++*/
+
+    $(".navHelpOut").click(function () {
+        //show nav expand div
+        $("#navHExpand").removeClass("hidden");
+        $("#navHExpand").animate({ height: "100%" }); //sets hight and animates
+        clickH = clickH + 1; //adds 1 to var
+
+        //hide nav expand div
+        if (clickH == 2) {
+            $("#navHExpand").addClass("hidden");
+            $("#navHExpand").animate({ height: "0%" }); //sets hight to 0%
+            clickH = 0;
+        }
+
+    });
 
 /*++ PREPARE ++*/
 
+    $(".navPrep").click(function () {
+        //show nav expand div
+        $("#navPExpand").removeClass("hidden");
+        $("#navPExpand").animate({ height: "100%" }); //sets hight and animates
+        clickP = clickP + 1; //adds 1 to var
+
+        //hide nav expand div
+        if (clickP == 2) {
+            $("#navPExpand").addClass("hidden");
+            $("#navPExpand").animate({ height: "0%" }); //sets hight to 0%
+            clickP = 0;
+        }
+
+    });
+
 /*+ MORE INFO +*/
+
+    $(".navMoreInfo").click(function () {
+        //show nav expand div
+        $("#navMIExpand").removeClass("hidden");
+        $("#navMIExpand").animate({ height: "100%" }); //sets hight and animates
+        clickM = clickM + 1; //adds 1 to var
+
+        //hide nav expand div
+        if (clickM == 2) {
+            $("#navMIExpand").addClass("hidden");
+            $("#navMIExpand").animate({ height: "0%" }); //sets hight to 0%
+            clickM = 0;
+        }
+
+    });
 
     
 });
